cbc548e521cfd0e29d176f30cbccf1ad447bf0bc
LeidyRoldan15 <ldroldan0516@gmail.com>
2026-02-07 08:15:57 -0500

    busqueda de peliculas

--- movie/views.py
+++ movie/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render
-#from django.http import HttpResponse
+from django.http import HttpResponse
+
+from .models import MOvie
 
 # Create your views here.
 
 def home(request):
     #return HttpResponse('<h1>Welcome to Home Page</h1>')
     #return render(request, 'home.html')
-    return render(request, 'home.html', {'name':'Leidy Roldan'}) 
+    #return render(request, 'home.html', {'name':'Leidy Roldan'}) 
+    searchTerm = request.GET.get('searchMovie')
+    if searchTerm:
+        Movie = MOvie.objects.filter(title__icontains=searchTerm)
+    else:
+        Movie = MOvie.objects.all()
+    return render(request, 'home.html', {'searchTerm':searchTerm, 'movies':Movie})
+
 def about(request):
+    #return HttpResponse('<h1>Welcome to About Page</h1>')
     return render(request, 'about.html')
